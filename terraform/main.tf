529a0269ccb0c6b48c104fd522da59bfa5d81cc3
Yang <yiyanghsu@gmail.com>
2026-06-08 23:30:21 -0400

    SOC Dashboard part 1

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,7 +35,7 @@ resource "aws_lambda_function" "soc_lambda" {
   source_code_hash = filebase64sha256("${path.module}/lambda/function.zip")
 
   depends_on = [
-  aws_cloudwatch_log_group.soc_log_group
+    aws_cloudwatch_log_group.soc_log_group
   ]
 
   tags = {
@@ -85,7 +85,7 @@ resource "aws_apigatewayv2_stage" "default_stage" {
       httpMethod     = "$context.httpMethod"
       routeKey       = "$context.routeKey"
       status         = "$context.status"
-      responseLength = "$context.responseLength"  
+      responseLength = "$context.responseLength"
     })
   }
 }
@@ -101,13 +101,13 @@ resource "aws_lambda_permission" "apigw" {
 
 resource "aws_cloudwatch_log_group" "soc_log_group" {
   name              = "/aws/lambda/soc-demo-lambda"
-  skip_destroy = false
+  skip_destroy      = false
   retention_in_days = 7
   # Security investigations often happen AFTER incidents
 }
 
 resource "aws_cloudwatch_log_metric_filter" "api_requests_filter" {
-  name = "ApiRequestCount"
+  name           = "ApiRequestCount"
   log_group_name = aws_cloudwatch_log_group.soc_log_group.name
 
   pattern = "API_REQUEST"
@@ -138,7 +138,7 @@ resource "aws_cloudwatch_metric_alarm" "high_api_requests" {
 
 resource "aws_sns_topic" "security_alerts" {
   name = "security-alerts"
-  kms_master_key_id = "alias/aws/sns"
+  #kms_master_key_id = "alias/aws/sns"
 }
 
 resource "aws_sns_topic_subscription" "email_alerts" {
@@ -163,18 +163,18 @@ resource "aws_iam_role" "incident_respones_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "incident_resoibse_basic_execution" {
-  role = aws_iam_role.incident_respones_role.name
+  role       = aws_iam_role.incident_respones_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_lambda_function" "incident_response_lambda" {
   function_name = "incident-response-lambda"
 
-  role = aws_iam_role.incident_respones_role.arn
+  role    = aws_iam_role.incident_respones_role.arn
   handler = "index.handler"
   runtime = "nodejs18.x"
 
-  filename = "${path.module}/incident-response/function.zip"
+  filename         = "${path.module}/incident-response/function.zip"
   source_code_hash = filebase64sha256("${path.module}/incident-response/function.zip")
 
   tags = {
@@ -186,58 +186,58 @@ resource "aws_lambda_function" "incident_response_lambda" {
 
 resource "aws_cloudwatch_log_group" "response_log_group" {
   name              = "/aws/lambda/incident-response-lambda"
-  skip_destroy = false
+  skip_destroy      = false
   retention_in_days = 7
   # Security investigations often happen AFTER incidents
 }
 
 resource "aws_sns_topic_subscription" "incident_response_subscription" {
   topic_arn = aws_sns_topic.security_alerts.arn
-  protocol = "lambda"
-  endpoint = aws_lambda_function.incident_response_lambda.arn
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.incident_response_lambda.arn
 }
 
 resource "aws_lambda_permission" "allow_sns_invoke" {
-  statement_id = "AllowExecutionFromSNS"
-  action = "lambda:InvokeFunction"
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.incident_response_lambda.function_name
-  principal = "sns.amazonaws.com"
-  source_arn = aws_sns_topic.security_alerts.arn
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.security_alerts.arn
 }
 
 resource "aws_cloudwatch_log_metric_filter" "suspicious_user_agent_filter" {
-  name = "SuspiciousUserAgentFilter"
+  name           = "SuspiciousUserAgentFilter"
   log_group_name = aws_cloudwatch_log_group.soc_log_group.name
 
   pattern = "SUSPICIOUS_USER_AGENT"
 
   metric_transformation {
-    name = "SuspiciousUserAgentMetric"
+    name      = "SuspiciousUserAgentMetric"
     namespace = "SOCProject"
-    value = "1"
+    value     = "1"
   }
 }
 
 resource "aws_cloudwatch_metric_alarm" "suspicious_user_agent_alarm" {
-  alarm_name = "SuspiciousUserAgentAlarm"
+  alarm_name          = "SuspiciousUserAgentAlarm"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 1
 
   metric_name = "SuspiciousUserAgentMetric"
-  namespace = "SOCProject"
+  namespace   = "SOCProject"
 
   period    = 60
   statistic = "Sum"
   threshold = 2
 
-  alarm_description = "Detect suspicious user-agent activity"
+  alarm_description  = "Detect suspicious user-agent activity"
   treat_missing_data = "notBreaching"
-  alarm_actions = [aws_sns_topic.security_alerts.arn]
+  alarm_actions      = [aws_sns_topic.security_alerts.arn]
 
 }
 
 resource "aws_cloudwatch_metric_alarm" "lambda_error_alarm" {
-  alarm_name = "LambdaErrorAlarm"
+  alarm_name          = "LambdaErrorAlarm"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 1
 
@@ -259,3 +259,112 @@ resource "aws_cloudwatch_metric_alarm" "lambda_error_alarm" {
   alarm_actions = [aws_sns_topic.security_alerts.arn]
 
 }
+
+resource "aws_cloudwatch_dashboard" "soc_dashboard" {
+  dashboard_name = "AWS-SOC-Dashboard"
+
+  dashboard_body = jsonencode({
+    widgets = [
+      {
+        type   = "metric"
+        x      = 0
+        y      = 0
+        width  = 12
+        height = 6
+
+        properties = {
+          title = "API Requests"
+
+          metrics = [
+            ["SOCProject", "ApiRequestMetric"]
+          ]
+
+          view   = "timeSeries"
+          region = "us-east-1"
+        }
+      },
+      {
+        type   = "metric"
+        x      = 12
+        y      = 0
+        width  = 12
+        height = 6
+
+        properties = {
+          title = "Suspicious User Agents"
+
+          metrics = [
+            ["SOCProject", "SuspiciousUserAgentMetric"]
+          ]
+
+          view   = "timeSeries"
+          region = "us-east-1"
+        }
+      },
+      {
+        type   = "metric"
+        x      = 0
+        y      = 6
+        width  = 12
+        height = 6
+
+        properties = {
+          title = "Lambda Errors"
+
+          metrics = [
+            [
+              "AWS/Lambda",
+              "Errors",
+              "FunctionName",
+              "soc-demo-lambda"
+            ]
+          ]
+
+          view   = "timeSeries"
+          region = "us-east-1"
+        }
+      },
+      {
+        type   = "metric"
+        x      = 12
+        y      = 6
+        width  = 12
+        height = 6
+
+        properties = {
+          title = "Lambda Invocations"
+
+          metrics = [
+            [
+              "AWS/Lambda",
+              "Invocations",
+              "FunctionName",
+              "soc-demo-lambda"
+            ]
+          ]
+
+          view   = "timeSeries"
+          region = "us-east-1"
+        }
+      },
+      {
+        type   = "alarm"
+        x      = 0
+        y      = 12
+        width  = 24
+        height = 6
+
+        properties = {
+          title = "Security Alarms"
+
+          alarms = [
+            aws_cloudwatch_metric_alarm.high_api_requests.arn,
+            aws_cloudwatch_metric_alarm.suspicious_user_agent_alarm.arn,
+            aws_cloudwatch_metric_alarm.lambda_error_alarm.arn
+          ]
+        }
+      }
+    ]
+  })
+}
+
